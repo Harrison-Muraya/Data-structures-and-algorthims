c7d07015a150bb7743b03dec624377efda76c2ef
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-08-27 23:54:15 +0300

    Added Node class and basic Link_list functionality, including isEmpty and size methods.

--- Link_list.py
+++ Link_list.py
@@ -8,6 +8,29 @@ class Node:
     
     def __repr__(self):
         return "node data is: %s" % self.data
+
+class Link_list:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None  
+    
+    def size(self):
+        current = self.head
+        count = 0
+
+        while current:
+            count +=1
+        
+        return count
+
+
     
 n1 =Node(10)
 print(n1)
+
+n2 = Node(20)
+n1.next_node=n2
+
+print(n1.next_node)
