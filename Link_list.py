688fb92b50b5e68c0fa2f4e1af68332230b1cd96
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-08-28 20:24:20 +0300

    Added Node class and LinkedList class with methods for adding nodes and checking size, implemented __repr__ for Node and LinkedList classes.

--- Link_list.py
+++ Link_list.py
@@ -1,37 +1,58 @@
-
 class Node:
     data = None
     next_node = None
 
     def __init__(self, data):
-        self.data = data
-    
-    def __repr__(self):
-        return "node data is: %s" % self.data
+        self.data = data 
 
-class Link_list:
+    def __repr__(self):
+        return "data in the node: %s" % self.data
+    
+
+class linked_list:
+
     def __init__(self):
         self.head = None
 
     def isEmpty(self):
-        return self.head == None  
+        return self.head == None
     
     def size(self):
-        current = self.head
+
         count = 0
+        current = self.head
 
         while current:
             count +=1
             current = current.next_node
-        
         return count
 
+    def add(self, data):
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
 
-    
-n1 =Node(10)
-print(n1)
+    def __repr__(self):
 
-n2 = Node(20)
-n1.next_node=n2
+        node = []
+        current = self.head
 
-print(n1.next_node)
+        while current:
+            if current == self.head:
+                node.append('head: %s' % current.data)
+            elif current.next_node is None:
+                node.append('tail: %s' % current.data)
+            else:
+                node.append('%s' % current.data)
+            current = current.next_node
+        return '->'.join(node)
+
+
+l = linked_list()
+l.add(11)
+l.add(20)
+l.add(90)
+l.add(110)
+print("Is Empty: ", l.isEmpty())
+print("list size: ", l.size())
+print(l)
