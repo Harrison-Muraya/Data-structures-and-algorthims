ed566f71d3d563211d2f69700ed7b619100b0daf
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-09-05 15:18:47 +0300

    `Refactored merge sort implementation: removed 38 lines of code`

--- merge_sort_recap.py
+++ merge_sort_recap.py
@@ -1,42 +1,4 @@
 
-def merge_sort(list):
-    if len(list) <= 1:
-        return list
-    left_half, right_half = split(list)
-    left = merge_sort(left_half)
-    right = merge_sort(right_half)
-
-    return merge(left, right)
-
-def split(list):
-    mid = len(list)//2
-    left = list[:mid]
-    right = list[mid:]
-    return left, right
-
-
-def merge(left, right):
-    result = []
-    i = 0
-    j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    
-    while i < len(left):
-        result.append(left[i])
-        i += 1
-
-    while j < len(right):
-        result.append(right[j])
-        j += 1
-
-    return result
 
 
 nums = [6,4,7,9,3,79,3,45,8,432,675,3,4,7,8,2,56,23,8,3,100,6,6,34]
