99faadabe6261c62d1da35d82ece2d750e255677
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-09-16 15:30:50 +0300

    Added elements to a stack and a count function, removed print statement

--- stacks.py
+++ stacks.py
@@ -1,4 +1,20 @@
 from collections import deque
 
 stack = deque()
-print(dir(stack))
+stack.append(3)
+stack.append(6)
+stack.append(8)
+stack.append(36)
+stack.append(33)
+
+def count(list):
+    count = 0
+    for item in list:
+        count += 1
+    return count
+
+
+print(stack)
+print(f"count: {count(stack)}")
+print(count(stack))
+# print(dir(stack))
